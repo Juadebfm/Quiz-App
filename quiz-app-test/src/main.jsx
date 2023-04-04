2fa0c662e37d2bd88ec76c5e8177f6665f7b07cd
Juadeb <juadebgabriel@gmail.com>
2023-04-04 14:39:38 +0100

    home page

--- quiz-app-test/src/main.jsx
+++ quiz-app-test/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
+// import "../node_modules/materialize-css/dist/js/materialize.min.js";
 import "./styles/style.scss";
 import App from "./App";
 
