2993111554cb13211c6fac7f26a2136f69321be5
Juadeb <juadebgabriel@gmail.com>
2023-04-05 14:51:31 +0100

    Created the right and wrong answer functionality

--- quiz-app-test/src/components/quiz/Quiz.jsx
+++ quiz-app-test/src/components/quiz/Quiz.jsx
@@ -2,17 +2,106 @@ import React, { Component } from "react";
 import { MdOutlineQuiz } from "react-icons/md";
 import { HiLightBulb } from "react-icons/hi";
 import { CiTimer } from "react-icons/ci";
+import M from "materialize-css";
+
+import questions from "../../../src/questions/QuestionsData.json";
+import isEmpty from "../../utils/isEmpty";
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      questions,
+      currentQuestion: {},
+      nextQuestion: {},
+      prevQuestion: {},
+      answer: "",
+      numberOfQuestions: 0,
+      numberOfAnsweredQuestions: 0,
+      currentQuestionIndex: 0,
+      score: 0,
+      correctAnswers: 0,
+      wrongAnswers: 0,
+      hints: 5,
+      fiftyFifty: 2,
+      usedFiftyFifty: false,
+      time: {},
+    };
   }
-  increaseCount = () => {
-    this.setState({
-      counter: 5,
-    });
+
+  componentDidMount() {
+    const { questions, currentQuestion, nextQuestion, prevQuestion } =
+      this.state;
+    this.displayQuestions(
+      questions,
+      currentQuestion,
+      nextQuestion,
+      prevQuestion
+    );
+  }
+
+  displayQuestions = (
+    questions = this.state.questions,
+    currentQuestion,
+    nextQuestion,
+    prevQuestion
+  ) => {
+    let { currentQuestionIndex } = this.state;
+    if (!isEmpty(this.state.questions)) {
+      questions = this.state.questions;
+      currentQuestion = questions[currentQuestionIndex];
+      nextQuestion = questions[currentQuestionIndex + 1];
+      prevQuestion = questions[currentQuestionIndex - 1];
+
+      const answer = currentQuestion.answer;
+
+      this.setState({
+        currentQuestion,
+        nextQuestion,
+        prevQuestion,
+        answer,
+      });
+    }
   };
+
+  handleOptionClick = (e) => {
+    if (e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
+      this.correctAnswer();
+    } else {
+      this.wrongAnswer();
+    }
+  };
+
+  correctAnswer = () => {
+    M.toast({
+      html: "Correct Answer!",
+      classes: "toast-valid",
+      displayLength: 1500,
+    });
+    this.setState((prevState) => ({
+      score: prevState.score + 1,
+      correctAnswer: prevState.correctAnswers + 1,
+      currentQuestionIndex: prevState.currentQuestionIndex + 1,
+      numberOfAnsweredQuestions: prevState.numberOfAnsweredQuestions + 1,
+    }));
+  };
+
+  wrongAnswer = () => {
+    navigator.vibrate(1000);
+    M.toast({
+      html: "Wrong Answer!",
+      classes: "toast-invalid",
+      displayLength: 1500,
+    });
+    this.setState((prevState) => ({
+      wrongAnswers: prevState.wrongAnswers + 1,
+      currentQuestionIndex: prevState.currentQuestionIndex + 1,
+      numberOfAnsweredQuestions: prevState.numberOfAnsweredQuestions,
+    }));
+  };
+
   render() {
+    const { currentQuestion } = this.state;
     return (
       <>
         <h1 className="main-topic">Quiz Test</h1>
@@ -29,21 +118,29 @@ class Quiz extends Component {
           </div>
           <div className="timer-option">
             <p>
-              <span>1 of 15</span>
+              <span>1 of 29</span>
               <span>
                 <CiTimer />
                 <span> 3:00</span>
               </span>
             </p>
           </div>
-          <h5>Google was founded in what year?</h5>
+          <h5>{currentQuestion.question}</h5>
           <div className="options-container">
-            <p className="option">1998</p>
-            <p className="option">1997</p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionA}
+            </p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionB}
+            </p>
           </div>
           <div className="options-container">
-            <p className="option">1999</p>
-            <p className="option">2000</p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionC}
+            </p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionD}
+            </p>
           </div>
           <div className="button-container">
             <button className="prev">Previous</button>
